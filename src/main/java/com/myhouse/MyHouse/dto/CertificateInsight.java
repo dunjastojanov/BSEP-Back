ec85e3b2f607c6008db41176504adcde065d68e1
Dunja <dunjastojanov2@gmail.com>
2023-04-03 21:08:12 +0200

    #3 Add fetching certificates

---
+++ src/main/java/com/myhouse/MyHouse/dto/CertificateInsight.java
@@ -0,0 +1,25 @@
+package com.myhouse.MyHouse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CertificateInsight {
+    private String commonName;
+    private String organization;
+    private String organizationUnit;
+
+    private String parentCommonName;
+    private String parentOrganization;
+    private String parentOrganizationUnit;
+
+    private String validFrom;
+    private String validTo;
+
+    private String alias;
+
+    private boolean verified;
+}
