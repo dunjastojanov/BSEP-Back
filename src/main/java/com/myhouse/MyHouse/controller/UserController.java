aed34c3f21c3f055fa0176d50e94a53bfaecef3f
Dunja <dunjastojanov2@gmail.com>
2023-04-30 23:53:33 +0200

    #17 Add get, delete, update roles and update real estates for user

--- src/main/java/com/myhouse/MyHouse/controller/UserController.java
+++ src/main/java/com/myhouse/MyHouse/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.myhouse.MyHouse.controller;
 
+import com.myhouse.MyHouse.dto.user.LoginDTO;
+import com.myhouse.MyHouse.dto.user.RegistrationDTO;
 import com.myhouse.MyHouse.dto.LoginDTO;
 import com.myhouse.MyHouse.dto.RegistrationDTO;
 import com.myhouse.MyHouse.dto.UserDTO;
@@ -7,10 +9,9 @@ import com.myhouse.MyHouse.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/user")
@@ -18,6 +19,39 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping
+    private ResponseEntity<?> getAll(@RequestParam int page, @RequestParam int size,
+                                     @RequestParam(required = false) String id,
+                                     @RequestParam(required = false) String name,
+                                     @RequestParam(required = false) String surname,
+                                     @RequestParam(required = false) String email,
+                                     @RequestParam(required = false) String role) {
+
+        return ResponseEntity.ok(userService.getAll(page, size, id, name, surname, email, role));
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<?> getById(@PathVariable String id) {
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> deleteById(@PathVariable String id) {
+        userService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/roles/{id}")
+    ResponseEntity<?> updateUserRoles(@PathVariable String id, @RequestParam List<String> roles) {
+        userService.updateUserRole(id, roles);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PutMapping("/realestates/{id}")
+    ResponseEntity<?> updateUserRealEstates(@PathVariable String id, @RequestParam List<String> realEstateIds) {
+        userService.updateUserRealEstates(id, realEstateIds);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     @PostMapping
     private ResponseEntity<?> register(@RequestBody RegistrationDTO registrationDTO) {
--- src/main/java/com/myhouse/MyHouse/service/UserService.java
+++ src/main/java/com/myhouse/MyHouse/service/UserService.java
@@ -1,21 +1,37 @@
 package com.myhouse.MyHouse.service;
 
-import com.myhouse.MyHouse.dto.LoginDTO;
-import com.myhouse.MyHouse.dto.RegistrationDTO;
-import com.myhouse.MyHouse.dto.UserDTO;
+import com.myhouse.MyHouse.dto.user.LoginDTO;
+import com.myhouse.MyHouse.dto.user.RegistrationDTO;
+import com.myhouse.MyHouse.dto.user.UserDTO;
+import com.myhouse.MyHouse.exceptions.NotFoundException;
+import com.myhouse.MyHouse.model.Role;
 import com.myhouse.MyHouse.model.User;
+import com.myhouse.MyHouse.repository.RealEstateRepository;
 import com.myhouse.MyHouse.repository.UserRepository;
 import com.myhouse.MyHouse.util.DataValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.query.Query;
 import org.owasp.encoder.Encode;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.*;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Autowired
+    private RealEstateRepository realEstateRepository;
 
     private final MailService mailService;
 
@@ -24,14 +40,25 @@ public class UserService {
     public void createUser(RegistrationDTO registrationDTO) {
         if (!DataValidator.isEmailValid(registrationDTO.getEmail())) return;
 
-        if (getUserByEmail(registrationDTO.getEmail()) != null) return;
+        if (getUserByEmail(registrationDTO.getEmail()) != null)
+            return;
+
+        User user = new User();
+        user.setName(registrationDTO.getName());
+        user.setEmail(registrationDTO.getEmail());
+        user.setPassword(registrationDTO.getPassword());
+        user.setSurname(registrationDTO.getSurname());
+        user.setRoles(List.of(Role.CLIENT));
+        user.setRealEstateIds(new ArrayList<>());
+        userRepository.save(user);
+        mailService.sendWelcomeEmail(user.getEmail(), user.getName(), user.getSurname());
 
         User u = userRepository.save(
                 new User(
-                    Encode.forHtml(registrationDTO.getName()),
-                    Encode.forHtml(registrationDTO.getSurname()),
-                    registrationDTO.getEmail(),
-                    passwordEncoder.encode(registrationDTO.getPassword())));
+                        Encode.forHtml(registrationDTO.getName()),
+                        Encode.forHtml(registrationDTO.getSurname()),
+                        registrationDTO.getEmail(),
+                        passwordEncoder.encode(registrationDTO.getPassword())));
 
         mailService.sendWelcomeEmail(u.getEmail(), u.getName(), u.getSurname());
     }
@@ -52,4 +79,78 @@ public class UserService {
     public String getUserIdByEmail(String email) {
         return getUserByEmail(email).getId();
     }
+
+    public Page<UserDTO> getAll(int page, int size, String id, String name, String surname, String email, String role) {
+        List<UserDTO> userDTOs = userRepository.findAll().stream().map(UserDTO::new).toList();
+        userDTOs = filterUserDTOs(id, name, surname, email, role, userDTOs);
+        return getPageFromList(page, size, userDTOs);
+    }
+
+    private static List<UserDTO> filterUserDTOs(String id, String name, String surname, String email, String role, List<UserDTO> userDTOs) {
+        if (id != null) {
+            userDTOs = userDTOs.stream()
+                    .filter(user -> user.getId().equals(id)).toList();
+        }
+
+        if (name != null) {
+            userDTOs = userDTOs.stream()
+                    .filter(user -> user.getName().equals(name)).toList();
+        }
+        if (surname != null) {
+            userDTOs = userDTOs.stream()
+                    .filter(user -> user.getSurname().equals(surname)).toList();
+        }
+        if (email != null) {
+            userDTOs = userDTOs.stream()
+                    .filter(user -> user.getEmail().equals(email)).toList();
+        }
+        if (role != null) {
+            userDTOs = userDTOs.stream()
+                    .filter(user -> user.getRoles().contains(Role.valueOf(role))).toList();
+        }
+        return userDTOs;
+    }
+
+    private static PageImpl<UserDTO> getPageFromList(int page, int size, List<UserDTO> entities) {
+        List<UserDTO> dtos = new ArrayList<>();
+        int total = entities.size();
+        int start = page * size;
+        int end = Math.min(start + size, total);
+        if (start <= end) {
+            dtos = entities.subList(start, end);
+        }
+        return new PageImpl<UserDTO>(dtos, PageRequest.of(page, size), total);
+    }
+
+    public UserDTO getById(String id) {
+        User user = findById(id);
+        return new UserDTO(user);
+
+    }
+
+    public User findById(String id) {
+        Optional<User> optional = userRepository.findById(id);
+        if (optional.isEmpty()) {
+            throw new NotFoundException("User not found.");
+        }
+        return optional.get();
+    }
+
+    public void deleteById(String id) {
+        userRepository.deleteById(id);
+    }
+
+    public void updateUserRole(String id, List<String> roles) {
+        User user = findById(id);
+        user.setRoles(roles.stream().map(Role::valueOf).toList());
+        userRepository.save(user);
+    }
+
+    public void updateUserRealEstates(String id, List<String> realEstateIds) {
+        User user = findById(id);
+        user.setRealEstateIds(realEstateRepository.findAllById(realEstateIds));
+        userRepository.save(user);
+    }
+
+
 }
